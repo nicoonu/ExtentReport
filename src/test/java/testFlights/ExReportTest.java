95ac0a6b653a8db6020703e93e86dab39d79de05
nicoonu <54816087+nicoonu@users.noreply.github.com>
2020-03-18 13:06:19 -0300

    .

--- src/test/java/testFlights/ExReportTest.java
+++ src/test/java/testFlights/ExReportTest.java
@@ -18,6 +18,8 @@ public class ExReportTest extends BaseTestFly {
 
         @BeforeTest
         public void setUpReport(){
+
+            /* Creo una instancia de reporte HTML y le indica la ruta donde quiero que me guarde el informe */
             htmlReporter = new ExtentHtmlReporter("./Reports/flights.html");
             extentReport = new ExtentReports();
             extentReport.attachReporter(htmlReporter);
@@ -25,6 +27,10 @@ public class ExReportTest extends BaseTestFly {
 
         @AfterMethod
         public void getResult(ITestResult result){
+
+            /* En esta funcion, utilizo la clase ExtentTest para registrar los resultados de mis test. */
+            /* Segun el estado del test, me va a mostrar diferentes mensajes */
+
             if(result.getStatus() == ITestResult.FAILURE){
                 extentTest.fail(MarkupHelper.createLabel(result.getName() + " Test Case Failed", ExtentColor.RED));
                 extentTest.fail(result.getThrowable());
@@ -35,7 +41,7 @@ public class ExReportTest extends BaseTestFly {
             }
 
             else{
-                extentTest.fail(MarkupHelper.createLabel(result.getName() + " Test Case Skipped", ExtentColor.BLUE));
+                extentTest.skip(MarkupHelper.createLabel(result.getName() + " Test Case Skipped", ExtentColor.BLUE));
                 extentTest.skip(result.getThrowable());
             }
         }
